79dfd36314ada5996c4de8c908dfb48ba5a1c25d
Gilbike <marton.dikacz@gmail.com>
2026-09-07 21:33:02 +0200

    refactor: extracted database connection string to variables

--- internal/database/database.go
+++ internal/database/database.go
@@ -2,11 +2,27 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"log/slog"
+	"strings"
 )
 
+const dbFileName = "app.db"
+
+var dbConnectParams = []string{
+	"_pragma=journal_mode(WAL)",
+	"_pragma=synchronous(NORMAL)",
+	"_pragma=busy_timeout(5000)",
+	"_pragma=foreign_keys(ON)",
+	"_time_format=sqlite",
+	"_texttotime=true",
+}
+
 func Connect() (*sql.DB, error) {
-	db, err := sql.Open("sqlite", "file:app.db?_pragma=journal_mode(WAL)&_pragma=synchronous(NORMAL)&_pragma=busy_timeout(5000)&_pragma=foreign_keys(ON)&_time_format=sqlite&_texttotime=true")
+	connParams := strings.Join(dbConnectParams, "&")
+	connString := fmt.Sprintf("file:%s?%s", dbFileName, connParams)
+
+	db, err := sql.Open("sqlite", connString)
 	if err != nil {
 		slog.Error("Failed to open connection to database", "error", err)
 		return nil, err
