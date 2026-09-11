dac4a7dc299f0d6dfcd9dd4492f329e09ff0d252
Gilbike <marton.dikacz@gmail.com>
2026-09-11 10:52:40 +0200

    feat(web): add styles and static font

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<title>Shelfd</title>
+</svelte:head>
+
 {@render children()}
