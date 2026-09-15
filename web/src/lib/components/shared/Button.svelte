ce9b50ff8d0a16bede2c6a5e198fdaa06d4a077a
Gilbike <marton.dikacz@gmail.com>
2026-09-15 09:02:44 +0200

    feat(web): add button component

---
+++ web/src/lib/components/shared/Button.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Button as BitsButton } from 'bits-ui';
+
+	const { children } = $props();
+</script>
+
+<BitsButton.Root class="interactive-primary w-full rounded p-1">
+	{@render children()}
+</BitsButton.Root>
