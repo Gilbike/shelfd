c221825b01683f3d7fb49d4636842dd65a35d299
Gilbike <marton.dikacz@gmail.com>
2026-09-15 09:35:06 +0200

    feat(web): add static login page

---
+++ web/src/routes/(auth)/auth/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Button from '$lib/components/shared/Button.svelte';
+	import FormInput from '$lib/components/shared/FormInput.svelte';
+	import { _ } from '$lib/i18n/index.svelte';
+</script>
+
+<div class="panel flex w-11/12 flex-col gap-1 sm:w-8/12 md:w-6/12 lg:w-1/4">
+	<h1 class="font-semibold">{_('auth.login')}</h1>
+	<FormInput id="username" name="username" label="Username" type="text" />
+	<FormInput id="password" name="password" label="Password" type="password" />
+	<Button>{_('auth.login')}</Button>
+</div>
+
+<style lang="postcss">
+	:global(body) {
+		@apply flex items-center justify-center;
+	}
+</style>
