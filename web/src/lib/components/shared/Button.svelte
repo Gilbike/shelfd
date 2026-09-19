e7b6ab62f84a0496dd141d6060d337e5164b68c4
Gilbike <marton.dikacz@gmail.com>
2026-09-19 16:34:17 +0200

    feat(web): add static sidebar

--- web/src/lib/components/shared/Button.svelte
+++ web/src/lib/components/shared/Button.svelte
@@ -1,17 +1,27 @@
 <script lang="ts">
 	import { Button as BitsButton } from 'bits-ui';
+	import clsx from 'clsx';
 	import type { Snippet } from 'svelte';
 	import type { HTMLButtonAttributes } from 'svelte/elements';
 
 	interface ButtonProps {
 		children: Snippet;
+		secondary?: boolean;
 	}
 
 	type Props = ButtonProps & HTMLButtonAttributes;
 
-	const { children, ...rest }: Props = $props();
+	const { children, secondary = false, ...rest }: Props = $props();
+
+	const styles = $derived(
+		clsx(
+			'rounded w-full p-1',
+			secondary ? 'interactive-secondary' : 'interactive-primary',
+			rest.class
+		)
+	);
 </script>
 
-<BitsButton.Root {...rest} class="interactive-primary w-full rounded p-1">
+<BitsButton.Root {...rest} class={styles}>
 	{@render children()}
 </BitsButton.Root>
---
+++ web/src/routes/(app)/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import Button from '$lib/components/shared/Button.svelte';
+	import { User } from '@lucide/svelte';
+
+	const { children } = $props();
+</script>
+
+<nav class="flex flex-col justify-between border-r border-r-border bg-surface p-4">
+	<div>
+	<Button
+	</div>
+	<Button secondary class="p-2">
+		<User />
+	</Button>
+</nav>
+<main class="flex-1">
+	{@render children()}
+</main>
+
+<style lang="postcss">
+	@reference '../layout.css';
+
+	:global(body) {
+		@apply flex w-full flex-row;
+	}
+</style>
