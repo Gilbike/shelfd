c392fb296e60be6e3d7a9bf076873cf9c9b7cca4
Gilbike <marton.dikacz@gmail.com>
2026-09-20 13:42:28 +0200

    feat(web): add navbar to layout

--- web/src/routes/(app)/+layout.svelte
+++ web/src/routes/(app)/+layout.svelte
@@ -1,16 +1,10 @@
 <script lang="ts">
-	import Button from '$lib/components/shared/Button.svelte';
-	import { User } from '@lucide/svelte';
+	import Navbar from '$lib/components/shared/Navbar.svelte';
 
 	const { children } = $props();
 </script>
 
-<nav class="flex flex-col justify-between border-r border-r-border bg-surface p-4">
-	<div></div>
-	<Button secondary class="p-2">
-		<User />
-	</Button>
-</nav>
+<Navbar />
 <main class="flex-1">
 	{@render children()}
 </main>
