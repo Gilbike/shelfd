c71b5608701bfbaa2123435d98dd2bef256dcd84
Gilbike <marton.dikacz@gmail.com>
2026-09-21 16:54:25 +0200

    feat(web): add book card for card display

---
+++ web/src/lib/components/books/BookCard.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { Book } from '$lib/api/types';
+	import { _ } from '$lib/i18n/index.svelte';
+
+	const { title, authors, pages, cover_url }: Book = $props();
+</script>
+
+<div
+	class="basis-11/12 overflow-hidden rounded border border-border bg-surface sm:basis-1/3 md:basis-1/4 lg:basis-1/5 xl:basis-1/6"
+>
+	<div class="p-10">
+		{#if cover_url === undefined}
+			<div class="flex aspect-1/1.5 w-full items-center justify-center bg-primary">
+				No cover image
+			</div>
+		{:else}
+			<img alt={title} src={cover_url} class="aspect-1/1.5 w-full" />
+		{/if}
+	</div>
+	<div class="border-t border-border p-2">
+		<p>{title}</p>
+		<p class="text-sm text-foreground/60">{authors.join(', ')}</p>
+		<p>{_('books.pages', { pages })}</p>
+	</div>
+</div>
--- web/src/routes/(app)/books/+page.svelte
+++ web/src/routes/(app)/books/+page.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
+	import BookCard from '$lib/components/books/BookCard.svelte';
+
 	const { data } = $props();
 </script>
 
 <div class="max-h-screen flex-1 overflow-hidden">
-	<div class="max-h-screen overflow-auto">
+	<div class="flex max-h-screen flex-row flex-wrap justify-evenly gap-2 overflow-auto p-4">
 		{#each data.books as book (book.id)}
-			<div>
-				<div>{book.title} by {book.authors.join(', ')}</div>
-				<div>{book.pages} pages</div>
-			</div>
+			<BookCard {...book} />
 		{/each}
 	</div>
 </div>
